3223ae1398d29fcafd5425b4acbfebaada49b258
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 11:46:26 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ cp pushover /usr/local/bin/pushover;
 chmod +x -R /usr/local/bin/pushover;
 rm -rf `dirname "$(realpath $0)"`;
 printf '\033[0;37m';
-printf '\nPushover CLI installed successfully! Run "pushover --help" for usage instructions.\n\n';
+printf '\nPushover CLI installed successfully! Run "pushover --help" for usage instructions.\n';
 printf '\033[0m\n';

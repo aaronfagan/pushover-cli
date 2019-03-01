949ff9fcd018bba03ca53693fd1eac5e33da16bf
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 15:26:01 -0700

    add version numbers to upgrade message

--- upgrade.sh
+++ upgrade.sh
@@ -3,7 +3,7 @@ VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /opt/pushover-cli/pushover)
 if [ "$1" == "$VERSION" ]
 then
 	printf '\033[0;37m'
-	printf "\nPushover CLI is already the latest version $1.\n"
+	printf "\nPushover CLI is already the latest version ($1).\n"
 	printf '\033[0m\n'
 else
 	cp /opt/pushover-cli/pushover /usr/local/bin/pushover

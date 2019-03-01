7f5951a5a181e51d9581def3c78a9c6c3be240f7
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 13:50:05 -0700

    add upgrade option

--- upgrade.sh
+++ upgrade.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-mkdir /opt/pushover-cli
-git clone git@github.com:aaronfagan/pushover-cli.git /opt
-cd /opt/pushover-cli
-chmod +x upgrade.sh
 cp pushover /usr/local/bin/pushover
 chmod +x -R /usr/local/bin/pushover
-rm -rf /opt/pushover-cli
-cd -
 printf '\033[0;37m'
 printf '\nPushover CLI upgraded successfully! Run "pushover --help" for usage instructions.\n'
 printf '\033[0m\n'

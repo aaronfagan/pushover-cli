3164d0f1609bd4fb59ad096f8845b79c1b90edb3
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 14:30:58 -0700

    add version numbers to upgrade message

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 source /usr/local/bin/pushover
-VERSION_OLD="$VERSION"
+echo $VERSION
 cp pushover /usr/local/bin/pushover
 chmod +x -R /usr/local/bin/pushover
 source /usr/local/bin/pushover
-VERSION_NEW="$VERSION"
+echo $VERSION"
 printf '\033[0;37m'
-printf "\nPushover CLI upgraded successfully from $VERSION_OLD to $VERSION_NEW! Run \"pushover --help\" for usage instructions.\n"
+printf "\nPushover CLI upgraded successfully from  to ! Run \"pushover --help\" for usage instructions.\n"
 printf '\033[0m\n'

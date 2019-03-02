ad3cce4de0e789cfbf39af805777aba2a1617b27
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 20:02:24 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp pushover /usr/local/bin/pushover
 chmod +x -R /usr/local/bin/pushover
+VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' pushover)
 rm -rf `dirname "$(realpath $0)"`
-VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 printf '\033[0;37m'
 printf '\nPushover CLI (v$VERSION) installed successfully! Run "pushover --help" for usage instructions.\n'
 printf '\033[0m\n'

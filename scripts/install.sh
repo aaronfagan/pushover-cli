230ab98e94d8c111ea3e8cf7180e1186773f1179
Aaron Fagan <aaron@thefagans.ca>
2019-03-02 15:32:55 -0700

    move functions to bottom of binary

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp ./bin/pushover /usr/local/bin/pushover
 chmod +x -R /usr/local/bin/pushover
-rm -rf `dirname "$(realpath $0)"`
+rm -rf ../../pushover-cli
 VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 echo -ne '\033[0;37m'
 echo -ne "\nPushover CLI (v$VERSION) installed successfully! Run 'pushover --help' for usage instructions.\n\n"

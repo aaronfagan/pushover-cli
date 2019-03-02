3117d9309195b6820cf96add4f38e252374d6b46
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 20:07:40 -0700

    add version number to install message

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ chmod +x -R /usr/local/bin/pushover
 rm -rf `dirname "$(realpath $0)"`
 VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 echo -ne '\033[0;37m'
-echo -ne "\nPushover CLI (v$VERSION) installed successfully! Run "pushover --help" for usage instructions.\n\n"
+echo -ne "\nPushover CLI (v$VERSION) installed successfully! Run \"pushover --help\" for usage instructions.\n\n"
 echo -ne '\033[0m'

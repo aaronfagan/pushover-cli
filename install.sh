557c3bf5df680ed02d91c76a8412f86e242b64b5
Aaron Fagan <aaron@thefagans.ca>
2019-03-01 17:42:25 -0700

    fixed: exit status codes

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp pushover /usr/local/bin/pushover;
-chmod +x -R /usr/local/bin/pushover;
-rm -rf `dirname "$(realpath $0)"`;
-printf '\033[0;37m';
-printf '\nPushover CLI installed successfully! Run "pushover --help" for usage instructions.\n';
-printf '\033[0m\n';
+cp pushover /usr/local/bin/pushover
+chmod +x -R /usr/local/bin/pushover
+rm -rf `dirname "$(realpath $0)"`
+printf '\033[0;37m'
+printf '\nPushover CLI installed successfully! Run "pushover --help" for usage instructions.\n'
+printf '\033[0m\n'
+

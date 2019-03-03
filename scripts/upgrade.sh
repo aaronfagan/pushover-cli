b04f9e68b66fc32fd5fd0712792c4577c661004f
Aaron Fagan <aaron@thefagans.ca>
2019-03-02 22:11:51 -0700

    move functions to bottom of binary

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /opt/pushover-cli/bin/pushover)
-if [ "$1" == "$VERSION" ]
+VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
+VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' /opt/pushover-cli/bin/pushover)
+if [ "$VERSION_OLD" == "$VERSION_NEW" ]
 then
 	echo -ne '\033[0;37m'
-	echo -ne "\nPushover CLI is already the latest version (v$1).\n\n"
+	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD).\n\n"
 	echo -ne '\033[0m'
 else
 	cp /opt/pushover-cli/bin/pushover /usr/local/bin/pushover
-	VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 	chmod +x /usr/local/bin/pushover
 	echo -ne '\033[0;37m'
-	echo -ne "\nPushover CLI upgraded successfully (v$1 => v$VERSION).\n\n"
+	echo -ne "\nPushover CLI upgraded successfully (v$VERSION_OLD => v$VERSION_NEW).\n\n"
 	echo -ne '\033[0m'
 fi

6c4922b1579a5dfbc13abc64f7059c82d9d224b7
Aaron Fagan <aaron@thefagans.ca>
2019-03-02 21:58:42 -0700

    move functions to bottom of binary

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -8,7 +8,7 @@ then
 else
 	cp /opt/pushover-cli/bin/pushover /usr/local/bin/pushover
 	VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
-	chmod +x -R /usr/local/bin/pushover
+	chmod +x /usr/local/bin/pushover
 	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI upgraded successfully (v$1 => v$VERSION).\n\n"
 	echo -ne '\033[0m'

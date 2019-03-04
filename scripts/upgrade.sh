0e8a4cce9cd850ad7cec78f2d53707fe1e1eff57
Aaron Fagan <aaron@thefagans.ca>
2019-03-04 16:29:40 -0700

    move to tmp dir

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
-VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' /tmp/pushover-cli/bin/pushover)
+DIR_TEMP="/tmp/pushover-cli/bin"
+DIR_BIN="/usr/local/bin"
+VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN/pushover)
+VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_TEMP/pushover)
 if [ "$VERSION_OLD" == "$VERSION_NEW" ]
 then
 	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD).\n\n"
 	echo -ne '\033[0m'
 else
-	cp /tmp/pushover-cli/bin/pushover /usr/local/bin/pushover
-	chmod +x /usr/local/bin/pushover
+	cp $DIR_TEMP/pushover $DIR_BIN/pushover
+	chmod +x $DIR_BIN/pushover
 	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI upgraded successfully (v$VERSION_OLD => v$VERSION_NEW).\n\n"
 	echo -ne '\033[0m'

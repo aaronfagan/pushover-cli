4f60d8674f60c4e450e154d80f6ea6b61a51538a
Aaron Fagan <aaron.fagan@telus.com>
2019-03-04 16:40:42 -0700

    Update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-DIR_BIN_TEMP="/tmp/pushover-cli/bin"
 DIR_BIN_LOCAL="/usr/local/bin"
-VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
+DIR_BIN_TEMP="/tmp/pushover-cli/bin"
 VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_LOCAL/pushover)
-if [ "$VERSION_NEW" == "$VERSION_OLD" ]
+VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
+if [ "$VERSION_OLD" == "$VERSION_NEW" ]
 then
 	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD).\n\n"

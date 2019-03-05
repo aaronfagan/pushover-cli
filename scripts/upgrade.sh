a4b4fefa4c4a9a35ff31644ea7afb9534509c7d2
Aaron Fagan <aaron@thefagans.ca>
2019-03-04 17:51:43 -0700

    pass tmp dir as variable

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 DIR_BIN_LOCAL="/usr/local/bin"
 DIR_BIN_TEMP="$1/bin"
-echo "VAR: $1"
 VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_LOCAL/pushover)
 VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
 if [ "$VERSION_OLD" == "$VERSION_NEW" ]
 then
 	echo -ne '\033[0;37m'
-	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD). Test\n\n"
+	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD).\n\n"
 	echo -ne '\033[0m'
 else
 	cp $DIR_BIN_TEMP/pushover $DIR_BIN_LOCAL/pushover

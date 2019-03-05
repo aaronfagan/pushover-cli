60bc7c74932179bba649c2c975408bcae4eb9298
Aaron Fagan <aaron@thefagans.ca>
2019-03-04 17:38:34 -0700

    move to tmp dir

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR_BIN_LOCAL="/usr/local/bin"
-DIR_BIN_TEMP="/tmp/pushover-cli/bin"
+DIR_BIN_TEMP="$1/bin"
 VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_LOCAL/pushover)
 VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
 if [ "$VERSION_OLD" == "$VERSION_NEW" ]

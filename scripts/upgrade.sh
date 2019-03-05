b9058ecf4bc72659044f5293173edce1dc3a0299
Aaron Fagan <aaron@thefagans.ca>
2019-03-04 18:46:41 -0700

    update upgrade script to force

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -3,15 +3,13 @@ DIR_BIN_LOCAL="/usr/local/bin"
 DIR_BIN_TEMP="$1/bin"
 VERSION_OLD=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_LOCAL/pushover)
 VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
+cp $DIR_BIN_TEMP/pushover $DIR_BIN_LOCAL/pushover
+chmod +x $DIR_BIN_LOCAL/pushover
+echo -ne '\033[0;37m'
 if [ "$VERSION_OLD" == "$VERSION_NEW" ]
 then
-	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI is already the latest version (v$VERSION_OLD).\n\n"
-	echo -ne '\033[0m'
 else
-	cp $DIR_BIN_TEMP/pushover $DIR_BIN_LOCAL/pushover
-	chmod +x $DIR_BIN_LOCAL/pushover
-	echo -ne '\033[0;37m'
 	echo -ne "\nPushover CLI upgraded successfully (v$VERSION_OLD => v$VERSION_NEW).\n\n"
-	echo -ne '\033[0m'
 fi
+echo -ne '\033[0m'

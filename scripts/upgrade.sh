b7f0aaab0f3eb333af2342f0d2c1562fb55bb374
Aaron Fagan <aaron.fagan@telus.com>
2019-03-07 07:50:45 -0700

    Update upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -4,7 +4,7 @@ DIR_BIN_TEMP="$1/bin"
 VERSION_NEW=$(awk -F'"' '/^VERSION=/ {print $2}' $DIR_BIN_TEMP/pushover)
 cp $DIR_BIN_TEMP/pushover $DIR_BIN_LOCAL/pushover
 chmod +x $DIR_BIN_LOCAL/pushover
-if [ "$VERSION_OLD" == "$VERSION_NEW" ]; then
+if [ "$2" == "$VERSION_NEW" ]; then
 	echo -ne "\nPushover CLI is already the latest version (v$2).\n\n"
 else
 	echo -ne "\nPushover CLI upgraded successfully (v$2 => v$VERSION_NEW).\n\n"

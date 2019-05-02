83b6ed107e51ef2e0c2c0f9e39191d4af4356a31
Aaron Fagan <aaron.fagan@telus.com>
2019-05-02 15:04:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ if [ "${BRANCH}" == "dev" ]; then BRANCH="develop"; fi
 git clone --branch "${BRANCH}" --depth 1 git@github.com:aaronfagan/pushover-cli.git /tmp/pushover-cli > /dev/null 2>&1
 cp /tmp/pushover-cli/pushover /usr/local/bin/pushover
 chmod +x /usr/local/bin/pushover
-echo -ne "done!"
 VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
+echo -ne "done!"
 echo -ne "\nPushover CLI (v${VERSION}) installed successfully! Run 'pushover --help' for usage instructions.\n\n"
 echo -ne '\033[0m'
 if [ "${2}" != "upgrade" ]; then rm -rf /tmp/pushover-cli; fi

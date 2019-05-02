9733a5b76b07f8f7a40ef0cf3a2e02a258b62ee0
Aaron Fagan <hello@aaronfagan.ca>
2019-05-02 11:59:26 -0600

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,6 @@ cp /tmp/pushover-cli/bin/pushover /usr/local/bin/pushover
 chmod +x /usr/local/bin/pushover
 rm -rf /tmp/pushover-cli
 VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
-#echo -ne "done!\n"
+echo -ne "done!\n"
 echo -ne "\nPushover CLI (v${VERSION}) installed successfully! Run 'pushover --help' for usage instructions.\n\n"
 echo -ne '\033[0m'

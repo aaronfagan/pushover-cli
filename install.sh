8c5f8d66551cedb286aadc3aff8b4cee34ede19b
Aaron <hello@aaronfagan.ca>
2019-05-02 12:10:32 -0600

    feat(install): move upgrade to process directly in binary

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 echo -ne '\033[0;37m'
 echo -ne "\nInstalling latest version..."
 git clone --depth 1 git@github.com:aaronfagan/pushover-cli.git /tmp/pushover-cli > /dev/null 2>&1
-cp /tmp/pushover-cli/bin/pushover /usr/local/bin/pushover
+cp /tmp/pushover-cli/pushover /usr/local/bin/pushover
 chmod +x /usr/local/bin/pushover
 rm -rf /tmp/pushover-cli
-VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 echo -ne "done!\n"
+VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 echo -ne "\nPushover CLI (v${VERSION}) installed successfully! Run 'pushover --help' for usage instructions.\n\n"
 echo -ne '\033[0m'

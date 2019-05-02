e59c1127b782f24d6c88e8277138d99fa5125116
Aaron <hello@aaronfagan.ca>
2019-05-02 15:00:48 -0600

    fix(install): unerary operator error

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo -ne '\033[0;37m'
-echo -ne "\nInstalling Pushover CLI..."
+echo -ne "\nDownloading Pushover CLI..."
 BRANCH="${1}"
 if [ -z "${BRANCH}" ]; then BRANCH="master"; fi
 if [ "${BRANCH}" == "dev" ]; then BRANCH="develop"; fi
 git clone --branch "${BRANCH}" --depth 1 git@github.com:aaronfagan/pushover-cli.git /tmp/pushover-cli > /dev/null 2>&1
 cp /tmp/pushover-cli/pushover /usr/local/bin/pushover
 chmod +x /usr/local/bin/pushover
-echo -ne "done!\n"
+echo -ne "done!"
 VERSION=$(awk -F'"' '/^VERSION=/ {print $2}' /usr/local/bin/pushover)
 echo -ne "\nPushover CLI (v${VERSION}) installed successfully! Run 'pushover --help' for usage instructions.\n\n"
 echo -ne '\033[0m'
